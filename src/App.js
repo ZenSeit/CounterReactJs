2d29132a3f1c81b682d46b10529874ada1dfa00e
Diego Becerra <diegofer_1110@hotmail.com>
2022-06-03 17:49:29 -0500

    Counter of clics done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './components/Boton';
+import Contador from './components/Contador';
+import {useState} from 'react';
 
 function App() {
+
+  const [numClics,setNumClics]=useState(0);
+
+    const manejarClic=()=>{
+      setNumClics(numClics+1);
+    }
+
+    const restarClic=()=>{
+      setNumClics(0);
+    }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='react-logo'>
+       <img className='react-img' src={'https://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png'} alt=" "/>
+     </div>
+     <div className="contenedor-principal">
+       <Contador numClics={numClics}/>
+       <div>
+       <Boton 
+       texto='Clic'
+       esBotonClic={true}
+       manejarClic={manejarClic}/>
+       <Boton texto='Reiniciar'
+       esBotonClic={false}
+       manejarClic={restarClic}
+       />
+       </div>
+      
+     </div>
     </div>
   );
 }
